10537db74bb1a50457d097aa9de772744966897c
Nilon123456789 <51246940+Nilon123456789@users.noreply.github.com>
2021-11-14 18:32:24 -0500

    Edit the receiver comment
    replace the '2' ... to 'ch1' ...

--- example/RC_mapped/RC_mapped.ino
+++ example/RC_mapped/RC_mapped.ino
@@ -10,7 +10,7 @@
 
 //Create an instance of a receiver
 //You can put up to 8 channels pin with one receiver instance
-//ex : Receiver receiver('2','3','4','5','6','7','8','9',);
+//ex : Receiver receiver('ch1','ch2','ch3','ch4','ch5','ch6','ch7','ch8',);
 Receiver receiver(2,3,4,5);
 
 //Channel min and max value
